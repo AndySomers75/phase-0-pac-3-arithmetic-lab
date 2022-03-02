78a28685e5ed794f064f20fd8858472cbf8bc2e4
Andrew <andrew@pureaquasprings.com>
2022-03-02 16:00:15 +1100

    completed

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a,b){
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+return a*b
+}
+function divide(x,y){
+return x/y
+}
+
+
+function increment(x){
+    return x+1
+}
+
+function decrement(x){
+    return x-1
+}
+
+function makeInt(a){
+    return parseInt(a,10)
+}
+
+function preserveDecimal(x){
+    return parseFloat(x)
+}
